e5e4890055b88d8d016289c6a494ed2a68d56d4f
thrivoydirect <thrivoydirect@gmail.com>
2025-09-18 20:36:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// Central configuration file for Thrivoy system
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx-kB8K0IATyXb6AFi2M8HAVn6yuTDLOyi_n6vlBLN2Qz5Mj_8e0zmBeJC0CPH8xl3u/exec";
